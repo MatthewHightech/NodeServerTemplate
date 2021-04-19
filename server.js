c89f58f007f05eb6f1af2b9caa9a932f20377691
MatthewHightech <Smoose007@gmail.com>
2021-04-19 12:29:16 -0700

    nps testing

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-const http = require('http'); 
-const path = require('path'); 
-const fs = require('fs'); 
+const express = require('express');
+//const app = express();
+const app = require("https-localhost")()
+const port = 5000;
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'public' , 'index.html'),
-         (err, content) => {
-             if (err) throw err; 
-            res.writeHead(200, { 'Content-Type': 'text/html'});
-            res.end(content); 
-        })
-    }
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT); 
-
+app.listen(port, () => console.log(`listening on port ${port}!`));
