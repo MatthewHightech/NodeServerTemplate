c71eea6cd10d1d5454cf7ba1768a013ddeacbea2
MatthewHightech <Smoose007@gmail.com>
2021-04-19 12:38:40 -0700

    heroku deploy express

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-//const app = express();
-const app = require("https-localhost")()
+const app = express();
+//const app = require("https-localhost")()
 const port = 5000;
 const path = require('path');
 
