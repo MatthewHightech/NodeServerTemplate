f4405b09d29ae17a7ba43e3b41152e4772abdc63
MatthewHightech <Smoose007@gmail.com>
2021-04-19 12:43:29 -0700

    heroku deploy update port

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 //const app = require("https-localhost")()
-const port = 5000;
+const port = process.env.PORT;
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.listen(port, () => console.log(`listening on port ${port}!`));
