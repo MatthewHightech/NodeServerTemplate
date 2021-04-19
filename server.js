c34b22ff65411cde4e8cfa107d626d30e783b598
MatthewHightech <Smoose007@gmail.com>
2021-04-19 12:52:02 -0700

    heroku deploy update port

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+app.listen(process.env.PORT || 8080, () => console.log("Server is running..."));
